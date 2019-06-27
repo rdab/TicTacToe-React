1915ef6580279692faf9fabb2c420f6bc157793e
rdab <rdamian.alfonso@riseup.net>
2019-06-27 14:45:25 -0400

    Bootstrap header component

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+        <header>
+            {this.props.text}
+        </header>
+        );
+    }
+}
+
+export default Header;
