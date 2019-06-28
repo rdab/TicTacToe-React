e5d2edea1d94b3e5feb8ae2ec8873e2efb8c5594
rdab <rdamian.alfonso@riseup.net>
2019-06-28 18:11:37 -0400

    Use action types constants in reducers

--- src/redux/redurcers/gameReducer.js
+++ src/redux/redurcers/gameReducer.js
@@ -1,8 +1,9 @@
 import { PlayerX, VALUES }  from "../../constants";
+import { PLAY_POSITION } from "../actionTypes";
 
 function gameReducer(state = VALUES, action) {
   switch (action.type) {
-    case "PLAY_POSITION":
+    case PLAY_POSITION:
       let newState = JSON.parse(JSON.stringify(state));
       let newValue = action.turn === PlayerX ? "X" : "0";
       newState[action.x][action.y] = newValue;
--- src/redux/redurcers/turnReducer.js
+++ src/redux/redurcers/turnReducer.js
@@ -1,8 +1,9 @@
 import { PlayerX, Player0 }  from "../../constants";
+import { PLAY_POSITION } from "../actionTypes";
 
 function turnReducer(state = PlayerX, action) {
   switch (action.type) {
-    case "PLAY_POSITION":
+    case PLAY_POSITION:
       return action.turn === PlayerX ? Player0 : PlayerX;
     default:
       return state;
