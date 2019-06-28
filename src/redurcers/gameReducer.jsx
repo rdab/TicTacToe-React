f939adeef64fcd5f32c98e0bcd30219868d06182
rdab <rdamian.alfonso@riseup.net>
2019-06-28 12:11:30 -0400

    Add game reducer

---
+++ src/redurcers/gameReducer.jsx
@@ -0,0 +1,15 @@
+import { PlayerX, VALUES }  from "../constants/constants";
+
+function gameReducer(state = VALUES, action) {
+  switch (action.type) {
+    case "PLAY_POSITION":
+      let newState = JSON.parse(JSON.stringify(state));
+      let newValue = action.turn === PlayerX ? "X" : "0";
+      newState[action.x, action.y] = newValue;
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default gameReducer;
