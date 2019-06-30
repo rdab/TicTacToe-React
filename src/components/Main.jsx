dd393c1b5f093c1a41ebc80f8b9425740cb182dd
rdab <rdamian.alfonso@riseup.net>
2019-06-30 15:07:30 -0400

    Add component Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { Game } from "./TicTacToe/Game";
+import { Home } from "./Home";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <nav>
+        <Route></Route>
+      </nav>
+    );
+  }
+}
+
+export default Main;
