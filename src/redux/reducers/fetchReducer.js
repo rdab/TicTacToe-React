354e4e670731504b5e6ef894914ce754cb97ba9e
rdab <rdamian.alfonso@riseup.net>
2019-06-30 23:16:47 -0400

    Fix switch-case non-fallthrought

--- src/redux/reducers/fetchReducer.js
+++ src/redux/reducers/fetchReducer.js
@@ -1,22 +1,24 @@
 import { FETCH } from "../../constants";
 import { FETCH_STATE_BEGIN, FETCH_STATE_SUCCESS, FETCH_STATE_FAILURE } from "../actionTypes";
 
-function fetchReducer(state=FETCH, action) {
+export default function fetchReducer(state=FETCH, action) {
   let newState;
   switch (action.type) {
     case FETCH_STATE_BEGIN:
       newState = JSON.parse(JSON.stringify(state));
       newState.fetching = true;
+      return newState;
     case FETCH_STATE_SUCCESS:
       newState = JSON.parse(JSON.stringify(state));
       newState.fetching = false;
       newState.finished = true;
+      return newState;
     case FETCH_STATE_FAILURE:
       newState = JSON.parse(JSON.stringify(state));
       newState.fetching = false;
       newState.error = action.error;
+      return newState;
     default:
-      newState = state;
+      return state;
   }
-  return newState;
 }
