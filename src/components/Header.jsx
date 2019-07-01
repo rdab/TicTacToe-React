e4aa9df5f8c660ac8794c13bf95633b6000d95c8
rdab <rdamian.alfonso@riseup.net>
2019-07-01 01:59:26 -0400

    Fix NavLinl import warning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavLink from "react-router-dom/NavLink";
+import { NavLink } from "react-router-dom";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
