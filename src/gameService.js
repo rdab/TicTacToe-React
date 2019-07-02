e245114395476696d5da78bdcbdde6334665280e
rdab <rdamian.alfonso@riseup.net>
2019-07-02 14:17:23 -0400

    rename ttt property

--- src/gameService.js
+++ src/gameService.js
@@ -4,10 +4,11 @@ export default class TicTacToe {
 
   constructor(player="", turn=PlayerX, values=VALUES,
               name="", uri=""){
-    this.playerName = player;
+    this.player = player;
     this.turn = turn;
     this.values = values;
     this.name = name;
     this.uri = uri;
+    this.updatedDate = null;
   }
 }
