cfe605426b9348aa35fc4ca56a0e8a8489f0eaaa
rdab <rdamian.alfonso@riseup.net>
2019-07-03 02:18:10 -0400

    Add routes path as constants

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,17 @@ import { Route } from "react-router-dom";
 import Game from "./TicTacToe/Game";
 import GameList from "./TicTacToe/GameList";
 import Home from "./Home";
+import { PATH } from "../constants";
 
 class Main extends React.Component {
   render() {
     return (
       <section className="mt-4">
-        <Route exact path='/' component={Home} />
-        <Route path='/new' component={Game} />
-        <Route path='/continue' component={() => <Game continue="true" />} />
-        <Route path='/games' component={GameList} />
+        <Route exact path={PATH.home} component={Home} />
+        <Route exact path={PATH.new} component={Game} />
+        <Route exact path={PATH.continue} component={Game} />
+        <Route path={`${PATH.continue}/:id`} component={Game} />
+        <Route exact path={PATH.games} component={GameList} />
       </section>
     );
   }
--- src/constants.js
+++ src/constants.js
@@ -19,3 +19,10 @@ export const HEADERS = {
 
 export const API = "https://api.myjson.com/bins/i216a";
 export const API_POST = "https://api.myjson.com/bins";
+
+export const PATH = {
+  home: "/",
+  new: "/new",
+  continue: "/continue",
+  games: "/games"
+}
