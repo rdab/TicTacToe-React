6a609d2848fc271513b628d85200b4c22dcd2251
rdab <rdamian.alfonso@riseup.net>
2019-07-03 15:32:34 -0400

    remove uri reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import gameReducer from "./gameReducer";
 import fetchReducer from "./fetchReducer";
-import uriReducer from "./uriReducer";
 import gameListReducer from "./games";
 
 export default (history) => combineReducers({
   router: connectRouter(history),
   fetch: fetchReducer,
-  uri: uriReducer,
   game: gameReducer,
   games: gameListReducer,
 })
