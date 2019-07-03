5fbcb6c558e471ae8a9236047c5bdd01ba2d993a
rdab <rdamian.alfonso@riseup.net>
2019-07-03 11:39:13 -0400

    Remove playername prop from store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,6 @@ import { connectRouter } from "connected-react-router";
 import turnReducer from "./turnReducer";
 import gameReducer, { gameReducerV2 } from "./gameReducer";
 import fetchReducer from "./fetchReducer";
-import playerReducer from "./playerReducer";
 import gameNameReducer from "./gameNameReducer";
 import uriReducer from "./uriReducer";
 import gameListReducer from "./games";
@@ -13,7 +12,6 @@ export default (history) => combineReducers({
   turn: turnReducer,
   values: gameReducer,
   fetch: fetchReducer,
-  playerName: playerReducer,
   gameName: gameNameReducer,
   uri: uriReducer,
   game: gameReducerV2,
